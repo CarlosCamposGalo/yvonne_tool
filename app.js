c107c8696c0a6ff6839fee9f33a85ba55c227ce8
Carlos Campos <ccampos@lingotek.com>
2019-08-27 11:59:23 +0800

    Added multiple divisions to be extracted

--- app.js
+++ app.js
@@ -3,36 +3,43 @@ import fs from 'fs'
 import stripBomStream from 'strip-bom-stream'
 import filter from './src/filter'
 import csvWriterInstances from './src/csvWriters'
-import extractionconf from './config/extraction.json'
+import divisions from './config/divisions'
 
 
-const HANDLE = (data) => {
-    if(filter(extractionconf.filters)(data)) {
-        const path = `${extractionconf.output.dir}/${extractionconf.output.filename}`
-        const writer = csvWriterInstances.build(path, Object.keys(data).map((key)=>{return {id: key, title: key}}))
-        writer.writeRecords(data)
+const HANDLE = (extractionconf) => {
+    return (data) => {
+        if(filter(extractionconf.filters)(data)) {
+            const path = `${extractionconf.output.dir}/${extractionconf.output.filename}`
+            const writer = csvWriterInstances.build(path, Object.keys(data).map((key)=>{return {id: key, title: key}}))
+            writer.writeRecords(data)
+        }
     }
 }
 
-
 const MAIN = () => {
+    for(const i in divisions) {
+        const extractionconf = divisions[i]
+        const destPath = `${extractionconf.output.dir}/${extractionconf.output.filename}`
+        const srcPath = `${extractionconf.input.dir}/${extractionconf.input.filename}`
+
+        console.log("SRC file:", srcPath)
+        console.log("DEST file:", destPath)
+
+        if(fs.existsSync(destPath)) {
+            fs.unlinkSync(destPath)
+            console.log(`Deleted destination path: ${destPath}`)
+        }
     
-    const destPath = extractionconf.output.dir + '/' + extractionconf.output.filename
-    const srcPath = extractionconf.input.dir + '/' + extractionconf.input.filename
-
-    console.log("SRC file:", srcPath)
-    console.log("DEST file:", destPath)
-
-    if(fs.existsSync(destPath)) {
-        fs.unlinkSync(destPath)
-        console.log(`Deleted destination path: ${destPath}`)
+        fs.createReadStream(srcPath)
+        .pipe(stripBomStream())
+        .pipe(csv_parser())
+        .on('data', HANDLE(extractionconf))
+        .on('end', () => csvWriterInstances.get(destPath)
+            .finalize(()=>{
+                console.log(`CSV written to path ${destPath}`)
+            })
+        );
     }
-   
-    fs.createReadStream(srcPath)
-    .pipe(stripBomStream())
-    .pipe(csv_parser())
-    .on('data', HANDLE)
-    .on('end', () => csvWriterInstances.finalize());
 }
 
 MAIN()
---
+++ config/divisions.js
@@ -0,0 +1,7 @@
+import mr_price_footwear from "./mr_price_footwear.json"
+import mr_price_footwear_copy from "./mr_price_footwear_copy.json"
+
+export default [
+    mr_price_footwear,
+    mr_price_footwear_copy
+]
